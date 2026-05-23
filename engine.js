96f4809c2465c9554916bb7eaaf4d18fcf04dc8f
peterlewis14-create <peter.lewis@sunwater.com.au>
2026-05-23 17:46:59 +1000

    Create engine.js

---
+++ engine.js
@@ -0,0 +1,8 @@
+document.getElementById("app").innerHTML = `
+  <p>Welcome. Click to begin.</p>
+  <button onclick="start()">Start</button>
+`;
+
+function start() {
+  alert("This is where your first question will go.");
+}
